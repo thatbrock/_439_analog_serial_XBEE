0a14f2633cdac8267ddee457fe87ad8a55511065
Brock <bcraft@uw.edu>
2018-05-19 09:32:00 -0700

    added xBee
    included xBee and minor corrections

--- _439_analog_serial_XBEE.ino
+++ _439_analog_serial_XBEE.ino
@@ -1,12 +1,23 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial xBee(2, 3);
+int prevValue = 0;
+
 void setup()
 {
   // start serial port at 9600 bps:
   Serial.begin(9600);
+  xBee.begin(9600);
   pinMode(A0, INPUT);
 }
 
 void loop() {
-  // read analog input, nd write its byte value to the serial port
+  // read analog input, and write its byte value to the serial port
   int analogValue = analogRead(0);
-  Serial.println(analogValue);
+  if (analogValue != prevValue) {
+    Serial.println(analogValue);
+    xBee.println(analogValue);
+    delay(100);
+    prevValue = analogValue;
+  }
 }
